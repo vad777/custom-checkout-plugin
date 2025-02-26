786d00f8b206c4627619d0b492483d71c1b56e08
Vadim <webstudialtd@ukr.net>
2025-02-26 15:22:31 +0200

    далены ненужные файлы из репозитория

--- wp-content/plugins/wp-my-product-webspark-main/templates/emails/admin-new-product.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!defined('ABSPATH')) exit;
-uidebug($product_link);
-?>
-
-<h2><?php echo esc_html($email_heading); ?></h2>
-<p><strong>Product Name:</strong> <?php echo esc_html($product_title); ?></p>
-<p><a href="<?php echo esc_url($product_link); ?>">View Product</a></p>
